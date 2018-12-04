df426dc78bb7fee3674d21a178409bef30a04f06
George Berezhnoy <gohabereg@users.noreply.github.com>
2018-12-04 20:28:12 +0300

    Update Toolbox API (#7)
    * Update Toolbox API
    
    * Bump version

--- src/index.js
+++ src/index.js
@@ -14,15 +14,6 @@ require('./index.css').toString();
  */
 class List {
   /**
-   * Should this tools be displayed at the Editor's Toolbox
-   * @returns {boolean}
-   * @public
-   */
-  static get displayInToolbox() {
-    return true;
-  }
-
-  /**
    * Allow to use native Enter behaviour
    * @returns {boolean}
    * @public
@@ -32,12 +23,17 @@ class List {
   }
 
   /**
-   * Get Tool icon's SVG
-   * @return {string}
-   * @public
+   * Get Tool toolbox settings
+   * icon - Tool icon's SVG
+   * title - title to show in toolbox
+   *
+   * @return {{icon: string, title: string}}
    */
-  static get toolboxIcon() {
-    return '<svg width="17" height="13" viewBox="0 0 17 13" xmlns="http://www.w3.org/2000/svg"> <path d="M5.625 4.85h9.25a1.125 1.125 0 0 1 0 2.25h-9.25a1.125 1.125 0 0 1 0-2.25zm0-4.85h9.25a1.125 1.125 0 0 1 0 2.25h-9.25a1.125 1.125 0 0 1 0-2.25zm0 9.85h9.25a1.125 1.125 0 0 1 0 2.25h-9.25a1.125 1.125 0 0 1 0-2.25zm-4.5-5a1.125 1.125 0 1 1 0 2.25 1.125 1.125 0 0 1 0-2.25zm0-4.85a1.125 1.125 0 1 1 0 2.25 1.125 1.125 0 0 1 0-2.25zm0 9.85a1.125 1.125 0 1 1 0 2.25 1.125 1.125 0 0 1 0-2.25z"/></svg>';
+  static get toolbox() {
+    return {
+      icon: '<svg width="17" height="13" viewBox="0 0 17 13" xmlns="http://www.w3.org/2000/svg"> <path d="M5.625 4.85h9.25a1.125 1.125 0 0 1 0 2.25h-9.25a1.125 1.125 0 0 1 0-2.25zm0-4.85h9.25a1.125 1.125 0 0 1 0 2.25h-9.25a1.125 1.125 0 0 1 0-2.25zm0 9.85h9.25a1.125 1.125 0 0 1 0 2.25h-9.25a1.125 1.125 0 0 1 0-2.25zm-4.5-5a1.125 1.125 0 1 1 0 2.25 1.125 1.125 0 0 1 0-2.25zm0-4.85a1.125 1.125 0 1 1 0 2.25 1.125 1.125 0 0 1 0-2.25zm0 9.85a1.125 1.125 0 1 1 0 2.25 1.125 1.125 0 0 1 0-2.25z"/></svg>',
+      title: 'List'
+    };
   }
 
   /**
